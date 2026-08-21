3ff533ba580ea672c886f8f85d90cc25174af944
varshinesubramani07-prog <varshinesubramani07@gmail.com>
2026-08-21 17:41:46 +0530

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -2,6 +2,8 @@ CREATE DATABASE RabiesGuard;
 
 USE RabiesGuard;
 
+
+-- Dog information
 CREATE TABLE Dogs (
     DogID INT PRIMARY KEY IDENTITY(1,1),
     RFID VARCHAR(50) UNIQUE,
@@ -13,20 +15,39 @@ CREATE TABLE Dogs (
     Location VARCHAR(200)
 );
 
-CREATE TABLE BiteReports (
-    ReportID INT PRIMARY KEY IDENTITY(1,1),
-    DogID INT,
-    CitizenName VARCHAR(100),
-    Phone VARCHAR(15),
-    BiteDate DATE,
-    Location VARCHAR(200),
-    FOREIGN KEY (DogID) REFERENCES Dogs(DogID)
-);
 
+-- Ward information
 CREATE TABLE Wards (
     WardID INT PRIMARY KEY,
     WardName VARCHAR(100),
     DogCount INT DEFAULT 0,
     VaccinatedDogs INT DEFAULT 0,
     BiteCases INT DEFAULT 0
+);
+
+
+-- Citizen bite reports
+CREATE TABLE BiteReports (
+    ReportID INT PRIMARY KEY IDENTITY(1,1),
+    DogID INT NULL,
+    CitizenName VARCHAR(100),
+    Phone VARCHAR(15),
+    BiteDate DATE,
+    Location VARCHAR(200),
+
+    FOREIGN KEY (DogID)
+        REFERENCES Dogs(DogID)
+);
+
+
+-- PEP vaccination schedule
+CREATE TABLE PEPSchedule (
+    PEPID INT PRIMARY KEY IDENTITY(1,1),
+    ReportID INT,
+    DoseName VARCHAR(20),
+    DueDate DATE,
+    Status VARCHAR(20) DEFAULT 'Pending',
+
+    FOREIGN KEY (ReportID)
+        REFERENCES BiteReports(ReportID)
 );
